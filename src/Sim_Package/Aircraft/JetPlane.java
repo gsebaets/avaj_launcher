fe662971e712564ec1f93aaa282cf03b362f2d05
coldheat <gsebaets@gmail.com>
2018-07-24 11:01:11 +0200

    one more step

--- src/Sim_Package/Aircraft/JetPlane.java
+++ src/Sim_Package/Aircraft/JetPlane.java
@@ -2,8 +2,11 @@ package Sim_Package.Aircraft;
 
 import Sim_Package.Coordinates;
 import Sim_Package.Weather.WeatherTower;
+import Sim_Package.WriteToFile;
 
 public class JetPlane extends Aircraft implements Flyable {
+
+    private WeatherTower weatherTower;
 
     JetPlane(String name, Coordinates coordinates) {
 
@@ -12,34 +15,46 @@ public class JetPlane extends Aircraft implements Flyable {
 
     public void updateConditions() {
 
-    }
+        int HeightCopy = this.coordinates.getHeight();
+        int LongCopy = this.coordinates.getLongitude();
+        int LatiCop = this.coordinates.getLatitude();
+        String weather = this.NweatherTower.getWeather(this.coordinates);
+
+
         switch (weather)
         {
             case "RAIN":
-                this.coordinates = new Coordinates();
+                this.coordinates = new Coordinates(LongCopy, LatiCop, HeightCopy - 5);
                 writer.writetofile("JetPlane#" + this.name + "(" + this.id + "): Let it RAIN, let it wash away my PAIN");
                 break;
             case "SNOW":
-                this.coordinates = new Coordinates();
+                this.coordinates = new Coordinates(LongCopy, LatiCop, HeightCopy - 15);
                 writer.writetofile("JetPlane#" + this.name + "(" + this.id + "): Elsa : Let it go, Let it goooo");
                 break;
             case "FOG":
-                this.coordinates = new Coordinates();
+                this.coordinates = new Coordinates(LongCopy, LatiCop, HeightCopy - 3);
                 writer.writetofile("JetPlane#" + this.name + "(" + this.id + "): What the FOG is going on?");
                 break;
             case "SUN":
-                this.coordinates = new Coordinates();
+                this.coordinates = new Coordinates(LongCopy + 2, LatiCop, HeightCopy + 4);
                 writer.writetofile("JetPlane#" + this.name + "(" + this.id + "): AAAAAAAA it blinds, it melts my ICE");
                 break;
         }
 
-    public void registerTower(WeatherTower weatherTower) {
-        writer.writetofile("Tower :" + "JetPlane#" + this.name + "(" + this.id + ") Registered");
+        public void registerTower (WeatherTower weatherTower)
+        {
+            writer.writetofile("Tower :" + "JetPlane#" + this.name + "(" + this.id + ") Registered");
+            this.NweatherTower = weatherTower;
+            weatherTower.register(this);
 
-    }
+        }
 
-    public void unregisterTower(WeatherTower weatherTower) {
-        writer.writetofile("Tower :" + "JetPlane#" + this.name + "(" + this.id + ") Unegistered");
+        public void unregisterTower (WeatherTower weatherTower)
+        {
+            writer.writetofile("Tower :" + "JetPlane#" + this.name + "(" + this.id + ") Unegistered");
+            this.NweatherTower = weatherTower;
+            weatherTower.unregister(this);
 
+        }
     }
 }
